4560f10c3db0407deba265512288fd2784932058
Renat Kalimulin <pianoparrot93@gmail.com>
2024-02-29 10:04:56 +0300

    Wizard: Add an ability to delete clusters from UI
    + removed unnecessary if statement
    + some logic was decomposed
    
    Resolves Issue #65

--- frontend/src/lib/hooks/api/appConfig.ts
+++ frontend/src/lib/hooks/api/appConfig.ts
@@ -1,6 +1,9 @@
 import { appConfigApiClient as api } from 'lib/api';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { ApplicationConfigPropertiesKafkaClusters } from 'generated-sources';
+import {
+  ApplicationConfig,
+  ApplicationConfigPropertiesKafkaClusters,
+} from 'generated-sources';
 import { QUERY_REFETCH_OFF_OPTIONS } from 'lib/constants';
 
 export function useAppInfo() {
@@ -15,6 +18,25 @@ export function useAppConfig() {
   return useQuery(['app', 'config'], () => api.getCurrentConfig());
 }
 
+function aggregateClusters(
+  cluster: ApplicationConfigPropertiesKafkaClusters,
+  existingConfig: ApplicationConfig,
+  initialName?: string,
+  deleteCluster?: boolean
+): ApplicationConfigPropertiesKafkaClusters[] {
+  const existingClusters = existingConfig.properties?.kafka?.clusters || [];
+
+  if (!initialName) {
+    return [...existingClusters, cluster];
+  }
+
+  if (!deleteCluster) {
+    return existingClusters.map((c) => (c.name === initialName ? cluster : c));
+  }
+
+  return existingClusters.filter((c) => c.name !== initialName);
+}
+
 export function useUpdateAppConfig({
   initialName,
   deleteCluster,
@@ -26,23 +48,13 @@ export function useUpdateAppConfig({
   return useMutation(
     async (cluster: ApplicationConfigPropertiesKafkaClusters) => {
       const existingConfig = await api.getCurrentConfig();
-      const existingClusters = existingConfig.properties?.kafka?.clusters || [];
 
-      let clusters: ApplicationConfigPropertiesKafkaClusters[] = [];
-
-      if (existingClusters.length > 0) {
-        if (!initialName) {
-          clusters = [...existingClusters, cluster];
-        } else if (!deleteCluster) {
-          clusters = existingClusters.map((c) =>
-            c.name === initialName ? cluster : c
-          );
-        } else {
-          clusters = existingClusters.filter((c) => c.name !== initialName);
-        }
-      } else {
-        clusters = [cluster];
-      }
+      const clusters = aggregateClusters(
+        cluster,
+        existingConfig,
+        initialName,
+        deleteCluster
+      );
 
       const config = {
         ...existingConfig,
