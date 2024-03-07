135055cb4515ffb7c05ee51134136e817a924fbd
Renat Kalimulin <103274228+Nilumilak@users.noreply.github.com>
2024-03-07 12:41:16 +0300

    FE: Create connector button throws 404 if KC cluster is inaccessible (#136)
    Co-authored-by: Roman Zabaluev <gpg@haarolean.dev>

--- frontend/src/components/common/Tooltip/Tooltip.tsx
+++ frontend/src/components/common/Tooltip/Tooltip.tsx
@@ -12,9 +12,15 @@ interface TooltipProps {
   value: React.ReactNode;
   content: string;
   placement?: Placement;
+  showTooltip?: boolean;
 }
 
-const Tooltip: React.FC<TooltipProps> = ({ value, content, placement }) => {
+const Tooltip: React.FC<TooltipProps> = ({
+  value,
+  content,
+  placement,
+  showTooltip = true,
+}) => {
   const [open, setOpen] = useState(false);
   const { x, y, refs, strategy, context } = useFloating({
     open,
@@ -28,7 +34,7 @@ const Tooltip: React.FC<TooltipProps> = ({ value, content, placement }) => {
       <div ref={refs.setReference} {...getReferenceProps()}>
         <S.Wrapper>{value}</S.Wrapper>
       </div>
-      {open && (
+      {showTooltip && open && (
         <S.MessageTooltip
           ref={refs.setFloating}
           style={{
