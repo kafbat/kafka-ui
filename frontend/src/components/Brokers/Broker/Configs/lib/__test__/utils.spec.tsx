f70c4e1f980943e2bc4bb4dd905cf19b0f01e565
alexeykozyurov <akozyurov@provectus.com>
2024-03-12 21:07:46 +0800

    tests

--- frontend/src/components/Brokers/Broker/Configs/lib/__test__/utils.spec.tsx
+++ frontend/src/components/Brokers/Broker/Configs/lib/__test__/utils.spec.tsx
@@ -1,8 +1,11 @@
 import {
+  getConfigDisplayValue,
   getConfigTableData,
   getConfigUnit,
 } from 'components/Brokers/Broker/Configs/lib/utils';
 import { ConfigSource } from 'generated-sources';
+import { render } from 'lib/testHelpers';
+import { ReactElement } from 'react';
 
 describe('getConfigTableData', () => {
   it('filters configs by search query and sorts by source priority', () => {
@@ -43,5 +46,43 @@ describe('getConfigUnit', () => {
     expect(getConfigUnit('log.retention.ms')).toBe('ms');
     expect(getConfigUnit('log.segment.bytes')).toBe('bytes');
     expect(getConfigUnit('compression.type')).toBeUndefined();
+  });
+});
+
+describe('getConfigDisplayValue', () => {
+  it('masks sensitive data with asterisks', () => {
+    const result = getConfigDisplayValue(true, 'testValue', undefined);
+    expect(result).toEqual({
+      displayValue: '**********',
+      title: 'Sensitive Value',
+    });
+  });
+
+  it('returns formatted bytes when unit is "bytes" and value is positive', () => {
+    const { container } = render(
+      getConfigDisplayValue(false, '1024', 'bytes').displayValue as ReactElement
+    );
+    expect(container).toHaveTextContent('1 KB');
+    expect(getConfigDisplayValue(false, '1024', 'bytes').title).toBe(
+      'Bytes: 1024'
+    );
+  });
+
+  it('returns value as is when unit is "bytes" but value is non-positive', () => {
+    const result = getConfigDisplayValue(false, '-1', 'bytes');
+    expect(result.displayValue).toBe('-1');
+    expect(result.title).toBe('-1');
+  });
+
+  it('appends unit to the value when unit is provided and is not "bytes"', () => {
+    const result = getConfigDisplayValue(false, '100', 'ms');
+    expect(result.displayValue).toBe('100 ms');
+    expect(result.title).toBe('100 ms');
+  });
+
+  it('returns value as is when no unit is provided', () => {
+    const result = getConfigDisplayValue(false, 'testValue', undefined);
+    expect(result.displayValue).toBe('testValue');
+    expect(result.title).toBe('testValue');
   });
 });
--- frontend/src/components/Brokers/Broker/Configs/lib/utils.tsx
+++ frontend/src/components/Brokers/Broker/Configs/lib/utils.tsx
@@ -96,6 +96,6 @@ export const getConfigDisplayValue = (
 
   return {
     displayValue: unit ? `${value} ${unit}` : value,
-    title: value.toString(),
+    title: unit ? `${value} ${unit}` : value,
   };
 };
