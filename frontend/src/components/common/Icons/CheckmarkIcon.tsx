d38053744bf4014387a205cca3956e5750fc70e5
brandon <2858207+igloo12@users.noreply.github.com>
2024-06-21 16:47:55 -0700

    Issue 372 - Icon color not updating with theme color

--- frontend/src/components/common/Icons/CheckmarkIcon.tsx
+++ frontend/src/components/common/Icons/CheckmarkIcon.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 
 const CheckmarkIcon: FC = () => {
   return (
@@ -13,18 +14,9 @@ const CheckmarkIcon: FC = () => {
     >
       <title>Checkmark</title>
       <desc>A line styled icon from Orion Icon Library.</desc>
-      <path
-        data-name="layer1"
-        fill="none"
-        stroke="#FFFFFF"
-        strokeMiterlimit="10"
-        strokeWidth="2"
-        d="M2 30l21 22 39-40"
-        strokeLinejoin="round"
-        strokeLinecap="round"
-      />
+      <path d="M25 48c-1.1 0-2.2-.4-3-1.2l-13-13c-1.6-1.6-1.6-4.2 0-5.8s4.2-1.6 5.8 0L25 38.8 49.2 14.6c1.6-1.6 4.2-1.6 5.8 0s1.6 4.2 0 5.8l-27 27c-.8.8-1.9 1.2-3 1.2z" />
     </svg>
   );
 };
 
-export default CheckmarkIcon;
+export default styled(CheckmarkIcon)``;
