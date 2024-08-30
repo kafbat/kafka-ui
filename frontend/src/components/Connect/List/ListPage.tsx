8ea84a3806b09733d8537609b250ca072efd6a4d
Alexis Demetriou <alex.demetriouad@gmail.com>
2024-08-30 16:24:28 +0300

    Removed unnecessary things from code

--- frontend/src/components/Connect/List/ListPage.tsx
+++ frontend/src/components/Connect/List/ListPage.tsx
@@ -2,17 +2,15 @@ import React, { useState, Suspense } from 'react';
 import useAppParams from 'lib/hooks/useAppParams';
 import { ClusterNameRoute, clusterConnectorNewRelativePath } from 'lib/paths';
 import ClusterContext from 'components/contexts/ClusterContext';
-import Search from 'components/common/Search/Search';
 import * as Metrics from 'components/common/Metrics';
 import PageHeading from 'components/common/PageHeading/PageHeading';
 import Tooltip from 'components/common/Tooltip/Tooltip';
-import { ControlPanelWrapper } from 'components/common/ControlPanel/ControlPanel.styled';
 import PageLoader from 'components/common/PageLoader/PageLoader';
 import { ConnectorState, Action, ResourceType } from 'generated-sources';
 import { useConnectors, useConnects } from 'lib/hooks/api/kafkaConnect';
 import { ActionButton } from 'components/common/ActionComponent';
-import * as S from './ListPage.styled';
 
+import * as S from './ListPage.styled';
 import List from './List';
 import ClustersList from './ClustersList';
 
@@ -92,9 +90,6 @@ const ListPage: React.FC = () => {
           </Metrics.Indicator>
         </Metrics.Section>
       </Metrics.Wrapper>
-      <ControlPanelWrapper hasInput>
-        <Search placeholder="Search by Connect Name, Status or Type" />
-      </ControlPanelWrapper>
       <S.Navbar>
         <S.Tab
           isActive={viewType === 'connectors'}
@@ -110,12 +105,8 @@ const ListPage: React.FC = () => {
         </S.Tab>
       </S.Navbar>
       <Suspense fallback={<PageLoader />}>
-        {viewType === 'connectors' && (
-          <List />
-        )}
-        {viewType === 'clusters' && (
-          <ClustersList />
-        )}
+        {viewType === 'connectors' && <List />}
+        {viewType === 'clusters' && <ClustersList />}
       </Suspense>
     </>
   );
