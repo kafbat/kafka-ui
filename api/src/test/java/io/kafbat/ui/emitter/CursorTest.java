303d7be529105a25bd0d9a11a89caf7c32b513ac
H@di <hadi@hadisafari.ir>
2024-09-17 11:36:32 +0330

    FE: fix a null pointer exception due to test fixtures (#550)

--- api/src/test/java/io/kafbat/ui/emitter/CursorTest.java
+++ api/src/test/java/io/kafbat/ui/emitter/CursorTest.java
@@ -164,7 +164,7 @@ class CursorTest extends AbstractIntegrationTest {
   }
 
   private Cursor.Tracking createCursor(ConsumerPosition position) {
-    return cursorsStorage.createNewCursor(createRecordsDeserializer(), position, m -> true, PAGE_SIZE, null);
+    return cursorsStorage.createNewCursor(createRecordsDeserializer(), position, m -> true, PAGE_SIZE, "CursorId");
   }
 
   private EnhancedConsumer createConsumer() {
