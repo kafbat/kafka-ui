f15fc925364efb98b1d82b01223a153bac3b39a4
François <francois.vandenpla@decathlon.com>
2024-11-12 14:54:58 +0100

    Issues/300: Other extractors handle regex as values.

--- api/src/main/java/io/kafbat/ui/service/rbac/extractor/OauthAuthorityExtractor.java
+++ api/src/main/java/io/kafbat/ui/service/rbac/extractor/OauthAuthorityExtractor.java
@@ -60,7 +60,7 @@ public class OauthAuthorityExtractor implements ProviderAuthorityExtractor {
             .filter(s -> s.getType().equals("user"))
             .peek(s -> log.trace("[{}] matches [{}]? [{}]", s.getValue(), principalName,
                 s.getValue().equalsIgnoreCase(principalName)))
-            .anyMatch(s -> principalName.matches(s.getValue())))
+            .anyMatch(s -> principalName != null && principalName.matches(s.getValue())))
         .map(Role::getName)
         .collect(Collectors.toSet());
 
@@ -94,11 +94,7 @@ public class OauthAuthorityExtractor implements ProviderAuthorityExtractor {
             .stream()
             .filter(s -> s.getProvider().equals(Provider.OAUTH))
             .filter(s -> s.getType().equals("role"))
-            .anyMatch(subject -> {
-              var roleName = subject.getValue();
-              return principalRoles.stream().anyMatch(s -> s.matches(subject.getValue()));
-            })
-        )
+            .anyMatch(subject -> principalRoles.stream().anyMatch(s -> s.matches(subject.getValue()))))
         .map(Role::getName)
         .collect(Collectors.toSet());
 
