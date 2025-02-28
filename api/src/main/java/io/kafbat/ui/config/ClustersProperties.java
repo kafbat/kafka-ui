76a83ea313959b8be1616c4e1787a1afc9685751
German Osin <german.osin@gmail.com>
2025-02-28 16:53:10 +0100

    fixed issues

--- api/src/main/java/io/kafbat/ui/config/ClustersProperties.java
+++ api/src/main/java/io/kafbat/ui/config/ClustersProperties.java
@@ -4,7 +4,6 @@ import io.kafbat.ui.model.MetricsConfig;
 import jakarta.annotation.PostConstruct;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -68,7 +67,7 @@ public class ClustersProperties {
 
     List<@Valid Masking> masking;
 
-    @Valid AuditProperties audit;
+    AuditProperties audit;
   }
 
   @Data
@@ -134,9 +133,7 @@ public class ClustersProperties {
   public static class SerdeConfig {
     @NotBlank
     String name;
-    @NotBlank
     String className;
-    @NotBlank
     String filePath;
     Map<String, Object> properties;
     String topicKeysPattern;
@@ -170,13 +167,11 @@ public class ClustersProperties {
   @NoArgsConstructor
   @AllArgsConstructor
   public static class AuditProperties {
-    @NotBlank
     String topic;
     Integer auditTopicsPartitions;
     Boolean topicAuditEnabled;
     Boolean consoleAuditEnabled;
-    @NotNull
-    LogLevel level;
+    LogLevel level = LogLevel.ALTER_ONLY;
     Map<String, String> auditTopicProperties;
 
     public enum LogLevel {
