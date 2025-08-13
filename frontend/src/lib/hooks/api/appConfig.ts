5b0d53474010bd161692028a2b1c68f34677f2ce
Mario Salazar de Torres <mario.sb93@gmail.com>
2025-08-13 10:37:35 +0200

    fix(fronted|auth): Fix login redirect
    - Fixed login redirect whenever the FQDN was containing `auth` in it.
    
    Fixes: #1261

--- frontend/src/lib/hooks/api/appConfig.ts
+++ frontend/src/lib/hooks/api/appConfig.ts
@@ -38,8 +38,9 @@ export function useAppInfo() {
       response = {};
     }
 
+    let url = new URL(data.raw.url);
     return {
-      redirect: data.raw.url.includes('auth'),
+      redirect: url.pathname.includes('auth'),
       response,
     };
   });
