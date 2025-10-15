63843334e775a9b1350141f5fe0b83bf423b8b9a
Pavel Makarichev <vixtir90@gmail.com>
2025-10-15 16:16:50 +0300

    FE: Fix race condition in cluster context

--- frontend/src/components/ClusterPage/ClusterPage.tsx
+++ frontend/src/components/ClusterPage/ClusterPage.tsx
@@ -42,7 +42,7 @@ const ClusterPage: React.FC = () => {
   const { clusterName } = useAppParams<ClusterNameRoute>();
   const appInfo = React.useContext(GlobalSettingsContext);
 
-  const { data } = useClusters();
+  const { data, isFetched } = useClusters();
   const contextValue = React.useMemo(() => {
     const cluster = data?.find(({ name }) => name === clusterName);
     const features = cluster?.features || [];
@@ -61,12 +61,14 @@ const ClusterPage: React.FC = () => {
       hasAclViewConfigured:
         features.includes(ClusterFeaturesEnum.KAFKA_ACL_VIEW) ||
         features.includes(ClusterFeaturesEnum.KAFKA_ACL_EDIT),
-      ftsEnabled: features.includes(ClusterFeaturesEnum.FTS_ENABLED),
-      ftsDefaultEnabled: features.includes(
-        ClusterFeaturesEnum.FTS_DEFAULT_ENABLED
-      ),
+      ftsEnabled: false,
+      ftsDefaultEnabled: false,
     };
   }, [clusterName, data]);
+
+  if (!isFetched) {
+    return <PageLoader />;
+  }
 
   return (
     <Suspense fallback={<PageLoader />}>
