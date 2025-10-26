d3f740e215124c90797a55f1928d47741cd63d7a
Leshe4ka <kozyurov.alexey@google.com>
2025-10-26 11:22:33 +0300

    topics connectors

--- frontend/src/components/Connect/List/ConnectorsTable/ConnectorsTable.tsx
+++ frontend/src/components/Connect/List/ConnectorsTable/ConnectorsTable.tsx
@@ -3,6 +3,7 @@ import { FullConnectorInfo } from 'generated-sources';
 import Table from 'components/common/NewTable';
 import { useLocalStoragePersister } from 'components/common/NewTable/ColumnResizer/lib';
 import { useQueryPersister } from 'components/common/NewTable/ColumnFilter';
+import { VisibilityState } from '@tanstack/react-table';
 
 import { connectorsColumns } from './connectorsColumns/columns';
 
@@ -12,11 +13,13 @@ const setRowId = (originalRow: FullConnectorInfo) =>
 type ConnectorsTableProps = {
   connectors: FullConnectorInfo[];
   columnSizingPersistKey?: string;
+  columnVisibility?: VisibilityState;
 };
 
 export const ConnectorsTable = ({
   connectors,
   columnSizingPersistKey = 'KafkaConnect',
+  columnVisibility,
 }: ConnectorsTableProps) => {
   const filterPersister = useQueryPersister(connectorsColumns);
   const columnSizingPersister = useLocalStoragePersister(
@@ -33,6 +36,7 @@ export const ConnectorsTable = ({
       emptyMessage="No connectors found"
       setRowId={setRowId}
       filterPersister={filterPersister}
+      columnVisibility={columnVisibility}
     />
   );
 };
