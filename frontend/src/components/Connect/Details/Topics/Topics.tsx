2ae770ffda5d2f8aef438343bf297f2295719786
Leshe4ka <kozyurov.alexey@google.com>
2025-10-29 21:12:49 +0500

    eslint fix

--- frontend/src/components/Connect/Details/Topics/Topics.tsx
+++ frontend/src/components/Connect/Details/Topics/Topics.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import useAppParams from 'lib/hooks/useAppParams';
-import {
-  clusterTopicPath,
-  RouterParamsClusterConnectConnector,
-} from 'lib/paths';
+import { RouterParamsClusterConnectConnector } from 'lib/paths';
 import { useConnector } from 'lib/hooks/api/kafkaConnect';
 import Table from 'components/common/NewTable';
 import { ColumnDef } from '@tanstack/react-table';
-import { Link } from 'react-router-dom';
-import { TableKeyLink } from 'components/common/table/Table/TableKeyLink.styled';
+
+import { TopicNameCell } from './cells/TopicNameCell';
+
+const columns: ColumnDef<{ topicName: string }>[] = [
+  {
+    header: 'Topic',
+    accessorKey: 'topicName',
+    cell: TopicNameCell,
+  },
+];
 
 const Topics = () => {
   const routerProps = useAppParams<RouterParamsClusterConnectConnector>();
@@ -16,24 +21,6 @@ const Topics = () => {
   const { data: raw } = useConnector(routerProps);
 
   const connector = { ...raw, topics: ['wikimedia.recentchange.connect'] };
-
-  const columns: ColumnDef<{ topicName: string }>[] = [
-    {
-      header: 'Topic',
-      accessorKey: 'topicName',
-      cell: ({ getValue }) => {
-        const topicName = getValue<string>();
-
-        return (
-          <TableKeyLink>
-            <Link to={clusterTopicPath(routerProps.clusterName, topicName)}>
-              {topicName}
-            </Link>
-          </TableKeyLink>
-        );
-      },
-    },
-  ];
 
   const tableData = (connector?.topics ?? []).map((topicName) => ({
     topicName,
