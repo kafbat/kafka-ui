c270c93eda463cbf57df1c98c0b7ae2b3fbca7de
Leshe4ka <kozyurov.alexey@google.com>
2025-11-10 15:32:58 +0500

    connector topics csv export

--- frontend/src/components/ConsumerGroups/Details/Details.tsx
+++ frontend/src/components/ConsumerGroups/Details/Details.tsx
@@ -22,6 +22,8 @@ import {
 import Tooltip from 'components/common/Tooltip/Tooltip';
 import { CONSUMER_GROUP_STATE_TOOLTIPS } from 'lib/constants';
 import ResourcePageHeading from 'components/common/ResourcePageHeading/ResourcePageHeading';
+import { exportTableCSV, TableProvider } from 'components/common/NewTable';
+import { Button } from 'components/common/Button/Button';
 
 import { TopicsTable } from './TopicsTable/TopicsTable';
 
@@ -46,82 +48,100 @@ const Details: React.FC = () => {
   const hasAssignedTopics = consumerGroup?.topics !== 0;
 
   return (
-    <div>
-      <div>
-        <ResourcePageHeading
-          text={consumerGroupID}
-          backTo={clusterConsumerGroupsPath(clusterName)}
-          backText="Consumers"
-        >
-          {!isReadOnly && (
-            <Dropdown>
-              <ActionDropdownItem
-                onClick={onResetOffsets}
-                permission={{
-                  resource: ResourceType.CONSUMER,
-                  action: Action.RESET_OFFSETS,
-                  value: consumerGroupID,
-                }}
-                disabled={!hasAssignedTopics}
-              >
-                Reset offset
-              </ActionDropdownItem>
-              <ActionDropdownItem
-                confirm="Are you sure you want to delete this consumer group?"
-                onClick={onDelete}
-                danger
-                permission={{
-                  resource: ResourceType.CONSUMER,
-                  action: Action.DELETE,
-                  value: consumerGroupID,
-                }}
-              >
-                Delete consumer group
-              </ActionDropdownItem>
-            </Dropdown>
-          )}
-        </ResourcePageHeading>
-      </div>
-      <Metrics.Wrapper>
-        <Metrics.Section>
-          <Metrics.Indicator label="State">
-            <Tooltip
-              value={
-                <Tag color={getTagColor(consumerGroup?.state)}>
-                  {consumerGroup?.state}
-                </Tag>
-              }
-              content={
-                CONSUMER_GROUP_STATE_TOOLTIPS[
-                  consumerGroup?.state || ConsumerGroupState.UNKNOWN
-                ]
-              }
-              placement="bottom-start"
-            />
-          </Metrics.Indicator>
-          <Metrics.Indicator label="Members">
-            {consumerGroup?.members}
-          </Metrics.Indicator>
-          <Metrics.Indicator label="Assigned Topics">
-            {consumerGroup?.topics}
-          </Metrics.Indicator>
-          <Metrics.Indicator label="Assigned Partitions">
-            {consumerGroup?.partitions?.length}
-          </Metrics.Indicator>
-          <Metrics.Indicator label="Coordinator ID">
-            {consumerGroup?.coordinator?.id}
-          </Metrics.Indicator>
-          <Metrics.Indicator label="Total lag">
-            {consumerGroup?.consumerLag}
-          </Metrics.Indicator>
-        </Metrics.Section>
-      </Metrics.Wrapper>
-      <ControlPanelWrapper hasInput style={{ margin: '16px 0 20px' }}>
-        <Search placeholder="Search by Topic Name" />
-      </ControlPanelWrapper>
+    <TableProvider>
+      {({ table }) => {
+        const handleExportClick = () => {
+          exportTableCSV(table, { prefix: 'connector-topics' });
+        };
 
-      <TopicsTable partitions={consumerGroup?.partitions ?? []} />
-    </div>
+        return (
+          <>
+            <div>
+              <ResourcePageHeading
+                text={consumerGroupID}
+                backTo={clusterConsumerGroupsPath(clusterName)}
+                backText="Consumers"
+              >
+                <Button
+                  buttonType="primary"
+                  buttonSize="M"
+                  onClick={handleExportClick}
+                >
+                  Export CSV
+                </Button>
+
+                {!isReadOnly && (
+                  <Dropdown>
+                    <ActionDropdownItem
+                      onClick={onResetOffsets}
+                      permission={{
+                        resource: ResourceType.CONSUMER,
+                        action: Action.RESET_OFFSETS,
+                        value: consumerGroupID,
+                      }}
+                      disabled={!hasAssignedTopics}
+                    >
+                      Reset offset
+                    </ActionDropdownItem>
+                    <ActionDropdownItem
+                      confirm="Are you sure you want to delete this consumer group?"
+                      onClick={onDelete}
+                      danger
+                      permission={{
+                        resource: ResourceType.CONSUMER,
+                        action: Action.DELETE,
+                        value: consumerGroupID,
+                      }}
+                    >
+                      Delete consumer group
+                    </ActionDropdownItem>
+                  </Dropdown>
+                )}
+              </ResourcePageHeading>
+            </div>
+            <Metrics.Wrapper>
+              <Metrics.Section>
+                <Metrics.Indicator label="State">
+                  <Tooltip
+                    value={
+                      <Tag color={getTagColor(consumerGroup?.state)}>
+                        {consumerGroup?.state}
+                      </Tag>
+                    }
+                    content={
+                      CONSUMER_GROUP_STATE_TOOLTIPS[
+                        consumerGroup?.state || ConsumerGroupState.UNKNOWN
+                      ]
+                    }
+                    placement="bottom-start"
+                  />
+                </Metrics.Indicator>
+                <Metrics.Indicator label="Members">
+                  {consumerGroup?.members}
+                </Metrics.Indicator>
+                <Metrics.Indicator label="Assigned Topics">
+                  {consumerGroup?.topics}
+                </Metrics.Indicator>
+                <Metrics.Indicator label="Assigned Partitions">
+                  {consumerGroup?.partitions?.length}
+                </Metrics.Indicator>
+                <Metrics.Indicator label="Coordinator ID">
+                  {consumerGroup?.coordinator?.id}
+                </Metrics.Indicator>
+                <Metrics.Indicator label="Total lag">
+                  {consumerGroup?.consumerLag}
+                </Metrics.Indicator>
+              </Metrics.Section>
+            </Metrics.Wrapper>
+            <ControlPanelWrapper hasInput style={{ margin: '16px 0 20px' }}>
+              <Search placeholder="Search by Topic Name" />
+            </ControlPanelWrapper>
+
+            <TopicsTable partitions={consumerGroup?.partitions ?? []} />
+          </>
+        );
+      }}
+    </TableProvider>
   );
 };
 
