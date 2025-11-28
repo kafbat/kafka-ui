a2c22744ba76470454dd5b82640d56d3ef37b72d
Chris Wood <christopherwoo@mo.co.uk>
2025-11-28 21:42:35 +0000

    issues/1506: Updated content-type to be passed through ResponseEntity and updated response of exposeClusterMetrics and exposeAllMetrics to be string

--- api/src/main/java/io/kafbat/ui/controller/PrometheusExposeController.java
+++ api/src/main/java/io/kafbat/ui/controller/PrometheusExposeController.java
@@ -4,12 +4,11 @@ import io.kafbat.ui.api.PrometheusExposeApi;
 import io.kafbat.ui.model.KafkaCluster;
 import io.kafbat.ui.service.StatisticsCache;
 import io.kafbat.ui.service.metrics.prometheus.PrometheusMetricsExposer;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
@@ -22,48 +21,46 @@ public class PrometheusExposeController extends AbstractController implements Pr
   private final StatisticsCache statisticsCache;
 
   @Override
-  public Mono<ResponseEntity<Void>> exposeAllMetrics(ServerWebExchange exchange) {
+  public Mono<ResponseEntity<String>> exposeAllMetrics(ServerWebExchange exchange) {
+
     String metrics = PrometheusMetricsExposer.exposeAllMetrics(
-        clustersStorage.getKafkaClusters()
-            .stream()
+        clustersStorage.getKafkaClusters().stream()
             .filter(KafkaCluster::isExposeMetricsViaPrometheusEndpoint)
             .collect(Collectors.toMap(
                 KafkaCluster::getName,
-                c -> statisticsCache.get(c).getMetrics()))
+                c -> statisticsCache.get(c).getMetrics()
+            ))
     ).getBody();
 
-    var response = exchange.getResponse();
-    response.getHeaders().add("Content-Type", "text/plain; version=0.0.4");
-
-    byte[] bytes = metrics.getBytes(StandardCharsets.UTF_8);
-    var buffer = response.bufferFactory().wrap(bytes);
-
-    return response.writeWith(Mono.just(buffer))
-        .doOnTerminate(response::setComplete)
-        .thenReturn(ResponseEntity.ok().build());
+    return Mono.just(
+        ResponseEntity
+            .ok()
+            .contentType(MediaType.parseMediaType("text/plain; version=0.0.4"))
+            .body(metrics)
+    );
   }
 
   @Override
-  public Mono<ResponseEntity<Void>> exposeClusterMetrics(String clusterName, ServerWebExchange exchange) {
+  public Mono<ResponseEntity<String>> exposeClusterMetrics(String clusterName, ServerWebExchange exchange) {
+
     Optional<KafkaCluster> cluster = clustersStorage.getClusterByName(clusterName);
+
     if (cluster.isEmpty() || !cluster.get().isExposeMetricsViaPrometheusEndpoint()) {
-      exchange.getResponse().setStatusCode(HttpStatus.NOT_FOUND);
-      return exchange.getResponse().setComplete()
-          .thenReturn(ResponseEntity.notFound().build());
+      return Mono.just(ResponseEntity.notFound().build());
     }
 
     String metrics = PrometheusMetricsExposer.exposeAllMetrics(
-        Map.of(clusterName, statisticsCache.get(cluster.get()).getMetrics())
+        Map.of(
+            clusterName,
+            statisticsCache.get(cluster.get()).getMetrics()
+        )
     ).getBody();
 
-    var response = exchange.getResponse();
-    response.getHeaders().add("Content-Type", "text/plain; version=0.0.4");
-
-    byte[] bytes = metrics.getBytes(StandardCharsets.UTF_8);
-    var buffer = response.bufferFactory().wrap(bytes);
-
-    return response.writeWith(Mono.just(buffer))
-        .doOnTerminate(response::setComplete)
-        .thenReturn(ResponseEntity.ok().build());
+    return Mono.just(
+        ResponseEntity
+            .ok()
+            .contentType(MediaType.parseMediaType("text/plain; version=0.0.4"))
+            .body(metrics)
+    );
   }
 }
