46ece641b5cd4e8acfab2aeb43ae293e3860422b
Pavel Makarichev <vixtir90@gmail.com>
2025-12-29 15:54:09 +0300

    FE: Fix lint

--- frontend/src/components/ACLPage/List/List.tsx
+++ frontend/src/components/ACLPage/List/List.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ColumnDef, Row } from '@tanstack/react-table';
 import Table from 'components/common/NewTable';
