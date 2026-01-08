fe8bc0731ad999b27c3eaf299d40f2f38dc46713
Dmitry Werner <grimekillah@gmail.com>
2026-01-08 12:39:49 +0500

    BE: Chore: Add unused imports check (#1610)

--- api/src/main/java/io/kafbat/ui/serdes/CustomSerdeLoader.java
+++ api/src/main/java/io/kafbat/ui/serdes/CustomSerdeLoader.java
@@ -7,8 +7,6 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Iterator;
