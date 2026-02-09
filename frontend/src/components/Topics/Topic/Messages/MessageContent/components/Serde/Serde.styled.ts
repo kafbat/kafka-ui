2e15682f6efd541cd1e7f1bcfc21eb940dd27dd8
Pavel Makarichev <vixtir90@gmail.com>
2026-02-09 22:45:49 +0300

    FE: remove redundant styles

--- frontend/src/components/Topics/Topic/Messages/MessageContent/components/Serde/Serde.styled.ts
+++ frontend/src/components/Topics/Topic/Messages/MessageContent/components/Serde/Serde.styled.ts
@@ -1,61 +1,5 @@
-import styled, { css } from 'styled-components';
-import * as SEditorViewer from 'components/common/EditorViewer/EditorViewer.styled';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
-export const Wrapper = styled.tr`
-  background-color: ${({ theme }) => theme.topicMetaData.backgroundColor};
-  & > td {
-    padding: 16px;
-    &:first-child {
-      padding-right: 1px;
-    }
-    &:last-child {
-      padding-left: 1px;
-    }
-  }
-`;
-
-export const Section = styled.div`
-  padding: 0 16px;
-  display: flex;
-  gap: 1px;
-  align-items: stretch;
-`;
-
-export const ContentBox = styled.div`
-  background-color: ${({ theme }) => theme.topicMetaData.backgroundColor};
-  padding: 24px;
-  border-radius: 8px 0 0 8px;
-  flex-grow: 3;
-  display: flex;
-  flex-direction: column;
-  & nav {
-    padding-bottom: 16px;
-  }
-  ${SEditorViewer.Wrapper} {
-    flex-grow: 1;
-  }
-`;
-export const DataCell = styled.td`
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  max-width: 350px;
-  min-width: 350px;
-`;
-export const ClickableRow = styled.tr`
-  cursor: pointer;
-`;
-export const MetadataWrapper = styled.div`
-  background-color: ${({ theme }) => theme.topicMetaData.backgroundColor};
-  padding: 24px;
-  border-radius: 0 8px 8px 0;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  min-width: 400px;
-`;
 
 export const Metadata = styled.span`
   display: flex;
@@ -78,32 +22,6 @@ export const MetadataMeta = styled.p`
   font-size: 12px;
 `;
 
-export const Tab = styled.button<{ $active?: boolean }>(
-  ({ theme, $active }) => css`
-    background-color: ${theme.secondaryTab.backgroundColor[
-      $active ? 'active' : 'normal'
-    ]};
-    color: ${theme.secondaryTab.color[$active ? 'active' : 'normal']};
-    padding: 6px 16px;
-    height: 32px;
-    border: 1px solid ${theme.layout.stuffBorderColor};
-    cursor: pointer;
-    &:hover {
-      background-color: ${theme.secondaryTab.backgroundColor.hover};
-      color: ${theme.secondaryTab.color.hover};
-    }
-    &:first-child {
-      border-radius: 4px 0 0 4px;
-    }
-    &:last-child {
-      border-radius: 0 4px 4px 0;
-    }
-    &:not(:last-child) {
-      border-right: 0;
-    }
-  `
-);
-
 export const SchemaLink = styled(Link)`
   cursor: pointer;
   color: ${({ theme }) => theme.link.color};
@@ -112,5 +30,3 @@ export const SchemaLink = styled(Link)`
     color: ${({ theme }) => theme.link.hoverColor};
   }
 `;
-
-export const Tabs = styled.nav``;
