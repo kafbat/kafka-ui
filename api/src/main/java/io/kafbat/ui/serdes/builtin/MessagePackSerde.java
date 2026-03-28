77f9165787e6b9ee898346646af8501f6221a945
YeeaaahMan <sergej.litvinchuk@gmail.com>
2026-03-28 15:20:06 +0200

    MessagePackSerde: support JSON to msgpack serializer + add unittests

--- api/src/main/java/io/kafbat/ui/serdes/builtin/MessagePackSerde.java
+++ api/src/main/java/io/kafbat/ui/serdes/builtin/MessagePackSerde.java
@@ -1,17 +1,23 @@
 package io.kafbat.ui.serdes.builtin;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.kafbat.ui.serde.api.DeserializeResult;
 import io.kafbat.ui.serde.api.Serde;
 import io.kafbat.ui.serdes.BuiltInSerde;
-import java.io.IOException;
 import java.util.Map;
 import org.msgpack.core.MessagePack;
 import org.msgpack.core.MessageUnpacker;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
 import org.msgpack.value.Value;
 
 
 public class MessagePackSerde implements BuiltInSerde {
   public static final String NAME = "MessagePack";
+
+  private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+  private static final ObjectMapper MSGPACK_MAPPER = new ObjectMapper(new MessagePackFactory());
 
   @Override
   public boolean canDeserialize(String topic, Serde.Target type) {
@@ -24,8 +30,25 @@ public class MessagePackSerde implements BuiltInSerde {
       try (MessageUnpacker unpacker = MessagePack.newDefaultUnpacker(data)) {
         Value value = unpacker.unpackValue();
         return new DeserializeResult(value.toString(), DeserializeResult.Type.STRING, Map.of());
-      } catch (IOException e) {
+      } catch (Exception e) {
         throw new IllegalArgumentException("Failed to deserialize MessagePack payload", e);
+      }
+    };
+  }
+
+  @Override
+  public boolean canSerialize(String topic, Serde.Target type) {
+    return true;
+  }
+
+  @Override
+  public Serde.Serializer serializer(String topic, Serde.Target type) {
+    return inputString -> {
+      try {
+        JsonNode node = JSON_MAPPER.readTree(inputString);
+        return MSGPACK_MAPPER.writeValueAsBytes(node);
+      } catch (JsonProcessingException e) {
+        throw new IllegalArgumentException("Failed to parse JSON payload", e);
       }
     };
   }
--- api/src/test/java/io/kafbat/ui/serdes/builtin/MessagePackSerdeTest.java
+++ api/src/test/java/io/kafbat/ui/serdes/builtin/MessagePackSerdeTest.java
@@ -1,6 +1,7 @@
 package io.kafbat.ui.serdes.builtin;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.kafbat.ui.serde.api.DeserializeResult;
 import io.kafbat.ui.serde.api.Serde;
@@ -12,7 +13,10 @@ import org.junit.jupiter.params.provider.EnumSource;
 
 public class MessagePackSerdeTest {
   private static final String TEST_STRING = "{\"items\":[null,true,2]}";
-  private static final byte[] TEST_BYTES = {
+  private static final String TEST_STRING_WITH_INDENTS = " { \n\t\"items\" : [ null, true , 2]\n}  ";
+  private static final String TEST_STRING_NOT_VALID_JSON = "{\"items\":[null,";
+  private static final byte[] TEST_DATA_WITH_MISSING_BYTE = { (byte) 0x93, 0x01, 0x02 };
+  private static final byte[] TEST_DATA = {
       (byte) 0x81,
       (byte) 0xa5,
       (byte) 0x69,
@@ -46,12 +50,6 @@ public class MessagePackSerdeTest {
 
   @ParameterizedTest
   @EnumSource
-  void canSerializeReturnsFalseForAllInput(Serde.Target type) {
-    assertThat(msgPackSerde.canSerialize("anyTopic", type)).isFalse();
-  }
-
-  @ParameterizedTest
-  @EnumSource
   void canDeserializeReturnsTrueForAllInputs(Serde.Target type) {
     assertThat(msgPackSerde.canDeserialize("anyTopic", type)).isTrue();
   }
@@ -60,9 +58,41 @@ public class MessagePackSerdeTest {
   @EnumSource
   void deserializesDataAsMessagePackBytes(Serde.Target type) {
     var deserializer = msgPackSerde.deserializer("anyTopic", type);
-    var result = deserializer.deserialize(new RecordHeadersImpl(), TEST_BYTES);
+    var result = deserializer.deserialize(new RecordHeadersImpl(), TEST_DATA);
     assertThat(result.getResult()).isEqualTo(TEST_STRING);
     assertThat(result.getType()).isEqualTo(DeserializeResult.Type.STRING);
     assertThat(result.getAdditionalProperties()).isEmpty();
+  }
+
+  @ParameterizedTest
+  @EnumSource
+  void deserializesNotValidDataThrowsException(Serde.Target type) {
+    var serializer = msgPackSerde.deserializer("anyTopic", type);
+    assertThatThrownBy(() -> serializer.deserialize(new RecordHeadersImpl(), TEST_DATA_WITH_MISSING_BYTE))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("Failed to deserialize MessagePack payload");
+  }
+
+  @ParameterizedTest
+  @EnumSource
+  void canSerializeReturnsTrueForAllInput(Serde.Target type) {
+    assertThat(msgPackSerde.canSerialize("anyTopic", type)).isTrue();
+  }
+
+  @ParameterizedTest
+  @EnumSource
+  void serializesDataAsMessagePackBytes(Serde.Target type) {
+    var serializer = msgPackSerde.serializer("anyTopic", type);
+    byte[] bytes = serializer.serialize(TEST_STRING_WITH_INDENTS);
+    assertThat(bytes).isEqualTo(TEST_DATA);
+  }
+
+  @ParameterizedTest
+  @EnumSource
+  void serializesNotValidDataThrowsException(Serde.Target type) {
+    var serializer = msgPackSerde.serializer("anyTopic", type);
+    assertThatThrownBy(() -> serializer.serialize(TEST_STRING_NOT_VALID_JSON))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("Failed to parse JSON payload");
   }
 }
