77d4832296011e9f30854cea294da54496fb0c39
Respirayson <raysonyeap4c@gmail.com>
2026-06-21 22:36:11 +0800

    fix: avoid invalid -1 port in Cognito logout redirect URL

--- api/src/main/java/io/kafbat/ui/config/auth/logout/CognitoLogoutSuccessHandler.java
+++ api/src/main/java/io/kafbat/ui/config/auth/logout/CognitoLogoutSuccessHandler.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.WebFilterExchange;
-import org.springframework.security.web.util.UrlUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.web.server.WebSession;
@@ -35,12 +34,8 @@ public class CognitoLogoutSuccessHandler implements LogoutSuccessHandler {
 
     final var requestUri = exchange.getExchange().getRequest().getURI();
 
-    final var fullUrl = UrlUtils.buildFullRequestUrl(requestUri.getScheme(),
-        requestUri.getHost(), requestUri.getPort(),
-        requestUri.getPath(), requestUri.getQuery());
-
     final UriComponents baseUrl = UriComponentsBuilder
-        .fromUriString(fullUrl)
+        .fromUri(requestUri)
         .replacePath("/")
         .replaceQuery(null)
         .fragment(null)
