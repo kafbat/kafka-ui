1cbe6f4b3ebd85a103f663aa1b778ea62a7544f6
Willem Serruys <willem.serruys@external.atlascopco.com>
2026-06-23 10:49:26 +0200

    fix: styling remarks

--- api/src/main/java/io/kafbat/ui/service/StatisticsService.java
+++ api/src/main/java/io/kafbat/ui/service/StatisticsService.java
@@ -78,12 +78,12 @@ public class StatisticsService {
   @NotNull
   private static Mono<LoadQuorumInfoResult> handleQuorumInfoErrors(Throwable quorumInfoFetchError) {
     if (quorumInfoFetchError instanceof UnsupportedVersionException) {
-      return Mono.just(new LoadQuorumInfoResult(Optional.empty(), ZOOKEEPER));
-    }
+      return Mono.just(new LoadQuorumInfoResult(Optional.empty(), ZOOKEEPER)); }
     else if (quorumInfoFetchError instanceof ClusterAuthorizationException) {
-      final String message = "Failed to fetch quorum info due to ClusterAuthorizationException." +
-          " In some managed Kafka offerings (e.g. Confluent Cloud), it is prohibited to fetch the quorum information from a cluster, regardless of your role configuration." +
-          " In that case, this error is expected behavior.";
+      final String message = "Failed to fetch quorum info due to ClusterAuthorizationException."
+          + " In some managed Kafka offerings (e.g. Confluent Cloud), "
+          + "it is prohibited to fetch the quorum information from a cluster, regardless of your role configuration."
+          + " In that case, this error is expected behavior.";
       log.warn(message, quorumInfoFetchError);
       return Mono.just(new LoadQuorumInfoResult(Optional.empty(), UNKNOWN));
     }
