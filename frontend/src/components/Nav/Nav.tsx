672e6e1da3d75c73c44074dec4094848968f389f
akshaya <akshaya.teekaraman@gmail.com>
2026-06-24 20:25:42 +0530

    Add search function in the UI to filter kafka clusters

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,6 +1,7 @@
-import React, { type FC } from 'react';
+import React, { type FC, useState } from 'react';
 import { useClusters } from 'lib/hooks/api/clusters';
 import useCurrentClusterName from 'lib/hooks/useCurrentClusterName';
+import Search from 'components/common/Search/Search';
 
 import * as S from './Nav.styled';
 import MenuItem from './Menu/MenuItem';
@@ -9,14 +10,31 @@ import ClusterMenu from './ClusterMenu/ClusterMenu';
 const Nav: FC = () => {
   const clusters = useClusters();
   const clusterName = useCurrentClusterName();
+  const [search, setSearch] = useState('');
+
+  const filteredClusters =
+    clusters.isSuccess && search.trim()
+      ? clusters.data.filter((c) =>
+          c.name.toLowerCase().includes(search.toLowerCase())
+        )
+      : clusters.data;
 
   return (
     <aside aria-label="Sidebar Menu">
       <S.List>
         <MenuItem variant="primary" to="/" title="Dashboard" />
       </S.List>
+      {clusters.isSuccess && clusters.data.length > 0 && (
+        <S.SearchWrapper>
+          <Search
+            placeholder="Search clusters..."
+            value={search}
+            onChange={setSearch}
+          />
+        </S.SearchWrapper>
+      )}
       {clusters.isSuccess &&
-        clusters.data.map((cluster) => (
+        filteredClusters?.map((cluster) => (
           <ClusterMenu
             key={cluster.name}
             name={cluster.name}
